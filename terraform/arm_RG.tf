fec1952babe1e05a0205c206cefe49791ac351e9
Michael Cho <mcho@grace.com.au>
2020-07-02 22:41:51 +1000

    Adding Resource Group

--- terraform/arm_RG.tf
+++ terraform/arm_RG.tf
@@ -1,13 +1,13 @@
 provider "azurerm" {
   # The "feature" block is required for AzureRM provider 2.x. 
   # If you are using version 1.x, the "features" block is not allowed.
-  version = "~>2.0"
+  version = "~>2.0.0"
   features {}
   subscription_id = "faaae9cc-6d6c-4370-adab-afc917594c0d"
 }
 
 # Create Resource Group
-resource "azurerm_resource_group" "static_website" {
-  name     = "AZ-${var.loc_prefix}-RG-${var.prefix}-${var.int_prefix}"
+resource "azurerm_resource_group" "static_blog" {
+  name     = "AZTESTTEST01"#"AZ- ${var.loc_prefix} -RG- ${var.prefix} - ${var.int_prefix}"
   location = "${var.location}"
 }
--- terraform/arm_SB.tf
+++ terraform/arm_SB.tf
@@ -1,15 +1,17 @@
 # Create Blob Storage For Static Website
 resource "azurerm_storage_account" "blog_blob" {
   name                      = "AZ-${var.loc_prefix}-SR-${var.prefix}-${var.int_prefix}"
-  resource_group_name       = azurerm_resource_group.static_website.name
-  location                  = azurerm_virtual_network.static_website.location
+  resource_group_name       = azurerm_resource_group.static_blog.name
+  location                  = azurerm_virtual_network.static_blog.location
   account_tier              = "Standard"
   account_kind              = "StorageV2"
   account_replication_type  = "LRS"
+  enable_https_traffic_only = true
   
   static_website {
-      index_document        = "index.html"
-      error_404_document    = "error.html"
+      enabled             = true
+      #index_document        = "index.html"
+      #error_404_document    = "error.html"
   }
 }
 
