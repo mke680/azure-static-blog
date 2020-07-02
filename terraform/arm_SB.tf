2ccc2c18dd5c50af10c0711d5984f0317e486fe5
Michael Cho <mcho@grace.com.au>
2020-07-02 14:05:48 +1000

    Adding RG, Storage and Variables.

--- terraform/arm_SB.tf
+++ terraform/arm_SB.tf
@@ -9,14 +9,6 @@ resource "azurerm_storage_account" "blog_blob" {
 
 resource "azurerm_storage_container" "blog_blob" {
   name                  = "AZ-${var.loc_prefix}-SC-${var.prefix}-${var.int_prefix}"
-  storage_account_name  = azurerm_storage_account.storage.name
+  storage_account_name  = azurerm_storage_account.blog_blob.name
   container_access_type = "private"
-}
-
-resource "azurerm_storage_blob" "blog_blob" {
-  name                   = "my-awesome-content.zip"
-  storage_account_name   = azurerm_storage_account.example.name
-  storage_container_name = azurerm_storage_container.example.name
-  type                   = "Block"
-  source                 = "some-local-file.zip"
 }
