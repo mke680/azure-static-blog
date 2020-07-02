73e155cb0381cd100dd8cbd08cde7936bed38d6e
Michael Cho <mcho@grace.com.au>
2020-07-02 15:40:47 +1000

    Added Storage with Static Website Config, RG and Variables

--- terraform/arm_SB.tf
+++ terraform/arm_SB.tf
@@ -1,14 +1,20 @@
 # Create Blob Storage For Static Website
-resource "azurerm_storage_account" "blog_blob" {
+resource "azurerm_storage_account" "static_website" {
   name                      = "AZ-${var.loc_prefix}-SR-${var.prefix}-${var.int_prefix}"
   resource_group_name       = azurerm_resource_group.rg.name
   location                  = azurerm_virtual_network.rg.location
   account_tier              = "Standard"
+  account_kind              = "StorageV2"
   account_replication_type  = "LRS"
+  
+  static_website {
+      index_document        = "index.html"
+      error_404_document    = "error.html"
+  }
 }
 
-resource "azurerm_storage_container" "blog_blob" {
+resource "azurerm_storage_container" "static_website" {
   name                  = "AZ-${var.loc_prefix}-SC-${var.prefix}-${var.int_prefix}"
-  storage_account_name  = azurerm_storage_account.blog_blob.name
+  storage_account_name  = azurerm_storage_account.static_website.name
   container_access_type = "private"
 }
